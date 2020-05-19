bf2dd1b6e1ab92959bb58cf36dade4ce37801865
Liza <bliza0812@gmail.com>
2020-05-19 14:10:39 +0400

    Решение

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args){
         // write your code here
+        Scanner in = new Scanner(System.in);
+        String line = in.nextLine();
+        System.out.println("Привет, "+line+"!");
     }
 }
